507502d89e3e7ce4100057e10b6ddbd25cedad4a
Alexandra <91855591+McAndra94@users.noreply.github.com>
2022-04-12 02:02:56 +0200

    11/04

--- backend/models/password.js
+++ backend/models/password.js
@@ -1,15 +1,12 @@
 const passwordValidator = require('password-validator');
-
-// Schéma de mot de passe plus sécure
 const passwordSchema = new passwordValidator();
 
-// Contraintes du mot de passe
 passwordSchema
-.is().min(6)                                    // Longueur minimun : 6
-.has().uppercase(1)                              // Doit avoir au moins une majuscule
-.has().lowercase(1)                              // Doit avoir au moins une minuscule
-.has().digits(1)                                 // Doit avoir au moins un chiffre
-.has().not().spaces()                           // Ne doit pas avoir d'espaces (pas sur que ce soit une bonne régle)
-.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist de valeurs à proscrire
+.is().min(6)                                    
+.has().uppercase(1)                              
+.has().lowercase(1)                              
+.has().digits(1)                                 
+.has().not().spaces()                           
+.is().not().oneOf(['Passw0rd', 'Password123']); 
 
 module.exports = passwordSchema;
